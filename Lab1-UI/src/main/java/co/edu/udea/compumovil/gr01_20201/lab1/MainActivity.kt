6db851aa0bf681048d83634cee57c6ce0f7dd6c6
maxdose <maxdose2@gmail.com>
2020-09-26 11:30:23 -0500

    Segundo Commit, ingresando informacion de los integrantes del proyecto, en la actividad inical.

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr01_20201/lab1/MainActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr01_20201/lab1/MainActivity.kt
@@ -1,5 +1,14 @@
 package co.edu.udea.compumovil.gr01_20201.lab1
-
+/*
+* PROYECTO DE LABORATORIO
+* UNIVERSIDAD DE ANTIOQUIA 2020-1
+* INTEGRANTES
+* Astrid Carolina Diaz Gomez
+* Max Hayver Falla Giraldo
+* Miriam Hasana Arango Vasquez
+*
+*
+* */
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
